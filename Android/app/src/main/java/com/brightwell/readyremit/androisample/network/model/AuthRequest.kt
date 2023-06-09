f3532969308d78c46c80f4d5868b36eb2ecb8040
Lucas Bordini <lucas.bordini@hotmail.com>
2023-06-09 16:04:18 -0300

    Android Sample
    Update Readme
    
    Android Sample

---
+++ Android/app/src/main/java/com/brightwell/readyremit/androisample/network/model/AuthRequest.kt
@@ -0,0 +1,11 @@
+package com.brightwell.readyremit.androisample.network.model
+
+import com.squareup.moshi.Json
+
+data class AuthRequest(
+    @Json(name = "client_id") var clientId: String,
+    @Json(name = "client_secret") var clientSecret: String,
+    @Json(name = "sender_id") var senderId: String,
+    @Json(name = "audience") val audience: String = "https://sandbox-api.readyremit.com",
+    @Json(name = "grant_type") val grantType: String = "client_credentials"
+)
